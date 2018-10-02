acfc594a53f95786694a5b2a40332fc46b49f9fc
vamsyprakky <um18048@stu.ximb.ac.in>
2018-10-02 16:03:29 +0530

    Class4_Session3

---
+++ WordCloud.R
@@ -0,0 +1,68 @@
+# library
+install.packages('wordcloud2')
+library(wordcloud2) 
+
+# have a look to the example dataset
+head(demoFreq)
+str(demoFreq)
+word_text=sample(c('Sahithi', 'barath', 'sarita','Surya','Vamsy','ritesh','rohith','CK','SVS'),1000,T, prob=(c(.15,.1,.1,.1,.15,.1,.1,.1,.1)))
+
+word_freq=sample(c(1:2), 1000,T)
+word_text=factor(word_text)
+word_final=data.frame(word_text,word_freq)
+
+wordcloud2(demoFreq, size=1)
+wordcloud2(word_final, size=0.6, shape ='star')
+?wordcloud2
+head(word_final)
+str(word_final)
+
+# Install
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+text <- readLines(file.choose())
+filePath <- "http://www.sthda.com/sthda/RDoc/example-files/martin-luther-king-i-have-a-dream-speech.txt"
+text <- readLines(filePath)
+docs <- Corpus(VectorSource(text))
+inspect(docs)
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d$freq
+d$word
+
+word_final$word_text
+word_final$word_freq
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
